c32267a90c2d248fa19295a2878502b6a1ebe9d0
Mehmood7 <mjhassan321@gmail.com>
2020-07-20 17:27:57 +0500

    understanding voice commands

--- app/src/main/java/com/example/homehaven/home.kt
+++ app/src/main/java/com/example/homehaven/home.kt
@@ -2,15 +2,21 @@ package com.example.homehaven
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.RecognizerIntent
+import android.speech.tts.TextToSpeech
 import android.view.View
-import android.widget.*
+import android.widget.AdapterView
+import android.widget.ImageView
+import android.widget.ListView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import java.util.*
+
 
 private lateinit var roomList:ListView
 private lateinit var speechView:ImageView
+private lateinit var roomNames:Vector<String>
 
 class home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +27,8 @@ class home : AppCompatActivity() {
         speechView = findViewById(R.id.voice_view)
 
         var db = dataStore(this)
-        var roomsAdaptor = roomAdapter(this, db.getRoomNames()!!)
+        roomNames = db.getRoomNames()!!
+        var roomsAdaptor = roomAdapter(this, roomNames)
         roomList.setAdapter(roomsAdaptor)
         roomList.setOnItemClickListener(
             object : AdapterView.OnItemClickListener {
@@ -65,9 +72,77 @@ class home : AppCompatActivity() {
 
         if (resultCode == Activity.RESULT_OK && requestCode == 72  && data != null){
             val resString = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
-            for (str in resString){
-                doToast(str)
+            var roomIndex = -1
+            var strRoomname = ""
+            var strOnOff = ""
+            var strDeviceName = ""
+            for ((i, room) in roomNames.withIndex()){
+                val roomName = room.replace('-',' ')
+                var j = 0
+                for (str in resString){
+                    if (str.contains(roomName, true)) {
+                        roomIndex = i
+                        strRoomname = roomName
+                    }
+                    //doToast(str)
+                    j++
+                }
             }
+
+            if(roomIndex != -1){
+                //doToast(roomNames.get(roomIndex))
+                var j = 0
+                var light = false
+                var fan = false
+                var socket = false
+
+                for (str in resString){
+                    if (str.contains("light", true)) {
+                        light = true
+                        strDeviceName = "light"
+                    }
+                    if (str.contains("fan", true)) {
+                        fan = true
+                        strDeviceName = "fan"
+                    }
+                    if (str.contains("socket", true)) {
+                        socket = true
+                        strDeviceName = "socket"
+                    }
+                    //doToast(str)
+                    j++
+                }
+
+                // ensures only one is true
+                var confused = !((light xor socket xor fan) xor (light && fan && socket))
+
+                if(!confused){
+                    var on = false
+                    var off = false
+                    for (str in resString){
+                        if (str.contains(" on", true)) {
+                            on = true
+                            strOnOff = "on"
+                        }
+                        if (str.contains(" off", true)) {
+                            off = true
+                            strOnOff = "off"
+                        }
+                        //doToast(str)
+                        j++
+                    }
+                    confused = !(on xor off)
+
+                    if (!confused){
+                        doToast(strRoomname+" "+strDeviceName+" "+strOnOff)
+                    }
+                    else
+                        doToast("Sorry unable to understand.(err1))")
+                }
+                else
+                    doToast("Sorry unable to understand.(err0)")
+            }
+            else doToast("Room not found")
         }
 
 
